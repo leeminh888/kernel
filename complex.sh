033e8abdbbd0b371672d43fc12395e66698f53c2
Wiley Lau <henloboii@protonmail.ch>
2021-04-05 13:09:45 +0800

    cleanup scripts
    Remove python3 purge cuz update-alternatives does what it does
    Move python2 installation to the big installer so it saves time

--- complex.sh
+++ complex.sh
@@ -88,13 +88,10 @@ fi
 # Setup
 echo "Setting up.."
 sudo apt-get -y update 
-sudo apt-get install bc cpio build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev rsync flex bison -y
+sudo apt-get install bc cpio build-essential zip curl libstdc++6 git wget python2 gcc clang libssl-dev rsync flex bison -y
 git config --global user.name "JamieHoSzeYui"
 git config --global user.email "wileylau@gmail.com"
 git config --global color.ui false
-sudo apt install python2 -y
-sudo apt-get purge python3* -y
-sudo apt-get remove --auto-remove python3* -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1 #Python2 supremacy
 python -V 
 
