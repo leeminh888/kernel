33d13fe8c0776283fe12c5eb0fcf75dca962c6ad
Wiley Lau <henloboii@protonmail.ch>
2021-04-05 10:59:01 +0800

    test

--- complex.sh
+++ complex.sh
@@ -75,13 +75,14 @@ fi
 
 # Setup
 echo "Setting up.."
-apt-get -y update && apt-get -y upgrade && apt-get install bc cpio build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev repo rsync flex bison -y
 git config --global user.name "JamieHoSzeYui"
 git config --global user.email "wileylau@gmail.com"
 git config --global color.ui false
 sudo apt-get purge python3*
 sudo apt-get remove --auto-remove python3*
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1 #Python2 supremacy
+python -V 
+exit
 
 echo "Cloning dependencies"
 git clone --depth=1 --single-branch $SAUCE build 
